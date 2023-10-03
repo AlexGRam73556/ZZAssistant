50363625ec671e83aa006419b63a41c44c192dd1
Emilio Alejandro Ramírez Gaytán <erg73556@udelasalle.edu.mx>
2023-10-03 12:08:51 -0600

    Send message from user number

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-// Array para almacenar los datos de los usuarios
-const usuarios = [
-    { nombre: "Alejandro", apellido: "Ramirez", telefono: "524776296253" },
-    { nombre: "Maria", apellido: "Perez", telefono: "524771234567" },
-    { nombre: "Alejandra", apellido: "Lopez", telefono: "524777654321" },
-    { nombre: "Pepe", apellido: "Hernandez", telefono: "524772467531" },
-    { nombre: "Ivanna", apellido: "Zuniga", telefono: "524771357642" },
-];
+function enviarMensajePorWhatsApp() {
+    const form = document.getElementById("userForm");
+    const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+    
+    let selectedUser = null;
 
-// Array para almacenar los checkboxes
-const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-
-// Manejar el cambio de selección de los checkboxes
-checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("change", () => {
-        // Desseleccionar todos los checkboxes excepto el seleccionado
-        checkboxes.forEach((cb, i) => {
-            if (i !== index) {
-                cb.checked = false;
-            }
-        });
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            selectedUser = {
+                nombre: checkbox.getAttribute("data-nombre"),
+                apellido: checkbox.getAttribute("data-apellido"),
+                telefono: checkbox.getAttribute("data-telefono"),
+            };
+        }
     });
-});
 
-// Función para mostrar el usuario seleccionado
-function mostrarUsuarioSeleccionado() {
-    const selectedCheckbox = checkboxes.find((checkbox) => checkbox.checked);
-
-    if (selectedCheckbox) {
-        const selectedIndex = checkboxes.indexOf(selectedCheckbox);
-        const selectedUser = usuarios[selectedIndex];
-        console.log("Datos del usuario seleccionado:");
-        console.log(`Nombre: ${selectedUser.nombre}`);
-        console.log(`Apellido: ${selectedUser.apellido}`);
-        console.log(`Teléfono: ${selectedUser.telefono}`);
+    if (selectedUser) {
+        const message = `Hola ${selectedUser.nombre} ${selectedUser.apellido}, este es un mensaje de WhatsApp. Mi número de teléfono es ${selectedUser.telefono}.`;
+        const whatsappURL = `https://wa.me/${selectedUser.telefono}?text=${encodeURIComponent(message)}`;
+        window.open(whatsappURL, '_blank');
     } else {
-        console.log("Ningún usuario seleccionado.");
+        alert("Por favor, selecciona un usuario antes de enviar el mensaje.");
     }
 }
+
+document.getElementById('whatsapp-button').addEventListener('click', enviarMensajePorWhatsApp);
